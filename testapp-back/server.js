2a9a32742fe43e1bc9ee5fd4dd8a4596302912b4
steveoneil <steve@steveoneil.com>
2017-03-11 19:37:50 -0500

    basic app functional - both front and back

---
+++ testapp-back/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + './../testapp-front/build'));
+
+app.get('/', (req, res) => {
+    res.send('It\'s working!...found "/" endpoint');
+
+});
+
+app.listen(PORT, () => {
+    console.log('Server running. Listening on Port:%s', PORT)
+    console.log('Stop with Ctrl+C')
+})
---
+++ testapp-front/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+const SERVERURL = 'http://localhost:8080';
+// const SERVERURL = '';
+
+class App extends Component {
+  constructor(){
+    super();
+      this.state = {
+        things: {
+          cool: ['coding', 'good food', 'fun people'],
+          not_cool: ['losing an hour of sleep', 'Trumpism']
+        },
+        stuff: "blah blah"
+      }
+      this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler () {
+    let _this = this;
+    axios.get(SERVERURL + '/')
+    .then (function (response) {
+      _this.setState({
+        stuff: response.data
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App container">
+        <Stuff clickHandler={this.clickHandler} stuff={this.state.stuff} />
+      </div>
+    );
+  }
+}
+
+class Stuff extends Component {
+
+  render() {
+    return(
+      <div className="Stuff"> 
+          <button className="btn-info" onClick={this.props.clickHandler}> Click me! </button>
+          <h4>{this.props.stuff}</h4>
+      </div>
+    )
+  }
+}
+
+export default App;
